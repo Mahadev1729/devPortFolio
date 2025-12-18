0c709fc3e391d507d68096019b9e59e06b27eed0
Mahadev1729 <devcampus17@gmail.com>
2025-12-18 20:21:49 +0530

    Fix import statement formatting in Portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,4 @@
-﻿import { motion } from "framer-motion";
+﻿﻿import { motion } from "framer-motion";
 import { SiLeetcode, SiHackerrank } from "react-icons/si";
 import { SiVercel, SiRender, SiPythonanywhere } from "react-icons/si";
 import { SiStreamlit } from "react-icons/si";
